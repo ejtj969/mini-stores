98e0d4896d1880f9bb2edd91c23efa61136bd1ad
Sabre3zero <108396167+Sabre3zero@users.noreply.github.com>
2026-05-07 00:38:56 +0300

    Product cards rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './variables.css'
 import styles from './App.module.css'
 import {AppCard} from './AppCard'
 
@@ -5,20 +6,32 @@ import {AppCard} from './AppCard'
 export default function App() {
   return (
     <>
-      <h1 className={styles.header}>MiniStore</h1>
-      <main>
-        {[
-          {title: 'Product1', text: 'Text1', date: '27.04'}, 
-          {title: 'Product2', text: 'Text2', date: '28.04'}, 
-          {title: 'Product3', text: 'Text3', date: '29.04'}
-        ].map(({title, text, date}) => (
-          <AppCard 
+      <body>
+        <header>
+          <h1 className={styles.header}>MiniStore</h1>
+        </header>
+
+        <main>
+          {[
+            {image: '/Product1.png', title: 'Product1', text: 'Some very cool description about Product1, also it is very very very very very very very long. Yeah it is definetely helpul huh? U rly still reading this? You are better doing... Oh, cutoff, see ya! man rly? why are you reading this XD Go get some sleep', tags: ['tag1', 'tag2']},
+            {image: '/Product2.png', title: 'Product2', text: 'Text2', tags: ['tag1', 'tag2', 'tag3', 'looong tag']},
+            {image: '/Product1.png', title: 'Product3', text: 'Text3', tags: ['tg', 'tag4']},
+            {image: '/Product1.png', title: 'Product4', text: 'Text4', tags: ['tag5']},
+            {image: '/Product1.png', title: 'Product5', text: 'Text5', tags: ['tag6', 'tag7', 'tag8', 'tag9', 'tag10']},
+            {image: '/Product1.png', title: 'Product6', text: 'Text6', tags: ['tag11', 'tag12', 'tag13']},
+            {image: '/Product1.png', title: 'Product7', text: 'Text7', tags: ['tag14', 'tag15']},
+            {image: '/Product1.png', title: 'Product8', text: 'Text8', tags: ['tag16']},
+            {image: '/Product1.png', title: 'Product9', text: 'Text9', tags: ['tag17', 'tag18', 'tag19', 'tag20']}
+          ].map(({image, title, text, tags}) => (
+          <AppCard
+            image={image}
             title={title}
             text={text}
-            date={date}
+            tags={tags}
           />
-        ))}
-      </main>
+          ))}
+        </main>
+      </body>
     </>
   );
 }
--- src/AppCard.jsx
+++ src/AppCard.jsx
@@ -1,12 +1,17 @@
 import styles from './AppCard.module.css'
 
 
-export function AppCard({title, text, date}){
+export function AppCard({image, title, text, tags}){
     return (
-            <section className={styles.card}>
-              <h2>{title}</h2>
-              <p>{text}</p>
-              <span>{date}</span>
-            </section>
+      <section className={styles.card}>
+        <img src={image}/>
+        <h2>{title}</h2>
+        <p>{text}</p>
+        <div className={styles.tag_container}>
+          {tags.map((tag) => 
+          <div className={styles.tag}>{tag}</div>
+          )}
+        </div>
+      </section>
     )
 }
